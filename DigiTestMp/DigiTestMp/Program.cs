8b9ae1864ca19c6627ad4fb9a81ee557dde8c587
Minna Pitkänen <minna.pitkanen@student.saimia.fi>
2019-09-04 11:39:57 +0300

    hello world task done

---
+++ DigiTestMp/DigiTestMp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DigiTestMp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
