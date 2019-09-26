ea688f750e3a6b312264cc4a699f48cd64208b8b
Minna Pitkänen <minna.pitkanen@student.saimia.fi>
2019-09-26 12:50:12 +0300

    ReadWriteExample Created

--- ExampleAverage/ExampleAverage/Program.cs
+++ ExampleAverage/ExampleAverage/Program.cs
@@ -6,7 +6,36 @@ namespace ExampleAverage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int summa = 0;
+            int i = 1;
+            Console.Write("Ohjelma laskee annettujen lukujen keskiarvon. Kuinka monta lukua syötät? ");
+            int lkm = int.Parse(Console.ReadLine());
+
+
+            while (i <= lkm)
+            {
+
+                int x = int.Parse(Console.ReadLine());
+                summa = summa + x;
+                i = i + 1;
+            }
+            if (lkm < 0)
+            {
+                double ka = summa / lkm;
+                double ka1 = ka;
+                NewMethod1(ka1);
+            }
+            else
+            {
+                Console.WriteLine("Lukuja ei ole syötetty!");
+            }
         }
+
+        private static void NewMethod1(double ka1)
+        {
+            NewMethod(ka1);
+        }
+
+    private static void NewMethod(double ka1) => Console.WriteLine("Keskiarvo on:"ka1);
     }
-}
+     
--- Factorial/Factorial/Program.cs
+++ Factorial/Factorial/Program.cs
@@ -6,7 +6,28 @@ namespace Factorial
     {
         static void Main(string[] args)
         {
+            int n = 0;
+            int fact = 1;
+
             Console.WriteLine("Kertoman laskenta");
+            Console.Write("Minkä luvun kertoma lasketaan? ");
+            n = int.Parse(Console.ReadLine());
+
+            if (n < 0)
+            {
+                Console.WriteLine("virheellinen syöte!");
+
+            }
+            else
+            {
+                while (n > 0)
+                {
+                    fact = fact * n;
+                    n = n - 1;
+                }
+                Console.WriteLine("Vastaus:" + fact);
+            }
         }
     }
+
 }
