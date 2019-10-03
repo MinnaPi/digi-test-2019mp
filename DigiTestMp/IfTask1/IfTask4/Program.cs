87b97389ccb62949011e5ea36e3ca9e3f4c5180c
Minna Pitkänen <minna.pitkanen@student.saimia.fi>
2019-10-03 09:48:50 +0300

    IfTask4 versioned

--- DigiTestMp/IfTask1/IfTask4/Program.cs
+++ DigiTestMp/IfTask1/IfTask4/Program.cs
@@ -7,6 +7,25 @@ namespace IfTask4
         static void Main(string[] args)
         {
             Console.WriteLine("Kolmen luvun lajittelu!");
+            //Tietojen  kysely
+            Console.Write("Syötä 1. luku: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("Syötä 2. luku: ");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.Write("Syötä 3. luku: ");
+            int z = int.Parse(Console.ReadLine());
+
+            // Logiikka, jolla ratkaistaan ongelma
+
+            
+
+
+
         }
     }
+
 }
+
+
