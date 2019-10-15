878b8a76b193c0d827c100856d81354794129af0
Minna Pitkänen <minna.pitkanen@student.saimia.fi>
2019-10-15 10:34:12 +0300

    PassWordAttempt is created

---
+++ DigiTestMp/PasswordQuist/PasswordQuist/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PasswordQuist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "qwerty1234";
+            int attemptCounter = 0;
+
+            Console.WriteLine("Syötä salasana.");
+            string userInput = Console.ReadLine();
+
+            while (userInput != password)
+            {
+                attemptCounter++;
+                if(attemptCounter>=3)
+                {
+                    Console.WriteLine("Tili on Lukittu!");
+                    return;//Ohjelman suoritus keskeytyy
+                }
+                Console.Write("Väärin! Yritä uudelleen: ");
+                userInput = Console.ReadLine();
+            }
+
+            Console.WriteLine("Salansana OIKEIN!");
+
+        }
+    }
+}
