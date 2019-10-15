f3d9de024feaff33c40cbd21293443e3539ad0fb
Minna Pitkänen <minna.pitkanen@student.saimia.fi>
2019-10-15 10:08:50 +0300

    IfTask32 is created

---
+++ DigiTestMp/IfTask32/IfTask32/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace IfTask32
+{
+    class Program
+    {
+        private static bool i;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee N lukujen summan");
+            Console.Write("Syötä luku");
+            int userInput = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <userInput; i++)
+            {
+                Console.WriteLine(i + 1);
+            }
+
+            Console.WriteLine("------------");
+
+            int j = 0; // Laskurin määritys
+            while(j<userInput) //Silmukan ehto. Silmukkaan tehdään niin pitkään kunnes ehto on epätosi.
+            {
+                Console.WriteLine(j + 1);
+                j++; //Laskurin kasvatus yhdellä
+                Console.WriteLine(j); // Tulostus
+
+                int n = 11;
+                do
+                {
+                    n++;
+                    Console.WriteLine(n);
+                } while (n < userInput);
+
+                Console.WriteLine("Ohjelman suoritus on päättynyt!");
+            }
+        }
+    }
+}
