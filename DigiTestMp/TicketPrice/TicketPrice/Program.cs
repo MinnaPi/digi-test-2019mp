940fc4fa6f8f29e8f28d497e42218897ded79961
Minna Pitkänen <minna.pitkanen@student.saimia.fi>
2019-10-28 13:32:31 +0200

    Digitest updated

--- DigiTestMp/TicketPrice/TicketPrice/Program.cs
+++ DigiTestMp/TicketPrice/TicketPrice/Program.cs
@@ -6,7 +6,40 @@ namespace TicketPrice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double discount = 0;
+            string isStudent = "";
+            string isMTKmember = "";
+            double ticketPrice = 16;
+            Console.WriteLine("Ticket Price");
+            Console.WriteLine("Oletko varusmies? K/E");
+            string isConscript = Console.ReadLine();
+            if (isConscript == "K") ;
+            {
+                discount = 0.5;
+            }
+            else
+            {
+                Console.WriteLine("Oletko opiskelija? K/E");
+                isStudent = Console.ReadLine();
+                if (isStudent == "K")
+                {
+                    Console.WriteLine("Oletko MTK:n jäsen? K/E");
+                    isMTKmember = Console.ReadLine();
+                    if (isMTKmember == "K")
+                    {
+                        discount = 0.6;
+                    }
+                    else
+                    {
+                        discount = 0.45;
+                    }
+
+
+                }
+                Console.WriteLine("Minkä ikäinen olet?");
+            
         }
+
     }
+}
 }
