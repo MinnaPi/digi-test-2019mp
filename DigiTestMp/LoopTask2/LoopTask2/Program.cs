eee1547c78188b75225fa888af3c2779cf4a62aa
Minna Pitkänen <minna.pitkanen@student.saimia.fi>
2019-10-28 13:30:41 +0200

    looptask2 is created

---
+++ DigiTestMp/LoopTask2/LoopTask2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LoopTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee N ensimmäistä lukua yhteen.");
+            Console.Write("Syötä luku: ");
+            int userInput = int.Parse(Console.ReadLine ());
+            int sumOfNumbers = 0;
+
+            // Ohjelman logiikka, silmukka
+            for (int i = 0; i < userInput; i++)
+            {
+                sumOfNumbers = sumOfNumbers + i;
+            }
+
+            Console.WriteLine($"Vastaus: {sumOfNumbers}");
+
+
+        }
+    }
+}
